23fb2b8400958869f25e7ce48d7276875b32b8d7
Jet Fontanilla <jetlogs@gmail.com>
2022-02-21 15:57:00 +0900

    initial build configuration

---
+++ preact.config.js
@@ -0,0 +1,12 @@
+export default (config, env, helpers) => {
+    config.node = {
+        fs: 'empty',
+    }
+    config.output.publicPath = process.env.BASE_URL || '/';
+    config.plugins.push(
+        new helpers.webpack.DefinePlugin({
+            PUBLIC_PATH: JSON.stringify(config.output.publicPath),
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        })
+    );
+};
